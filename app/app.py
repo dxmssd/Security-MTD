b69e2771b50b29ad4133761a3b526c3a34ead6b1
dxmssd <nicolasmanriquez022@gmail.com>
2026-03-19 20:34:16 -0300

    update arquitecture

--- app/app.py
+++ app/app.py
@@ -1,24 +1,53 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
 import os
-import socket
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({
-        "status": "secure",
-        "message": "MTD Node Active",
-        "node_info": {
-            "hostname": socket.gethostname(),
-            "ip": socket.gethostbyname(socket.gethostname()),
-            "rotation_id": os.getenv('MTD_ROTATION_ID', 'initial_state')
-        }
-    })
+#modelo y escalador
+MODEL_PATH = "model_ghost_node.pkl"
+SCALER_PATH = "scaler_ghost_node.pkl"
+                                    
 
+try:
+    model = joblib.load(MODEL_PATH)
+    scaler = joblib.load(SCALER_PATH)
+    print("Modelo y escalador cargados correctamente.")
+    
+except Exception as e:
+    print(f"Error al cargar el modelo o el escalador: {e}")
+    model = None
+
+#rute for kubernetes
 @app.route('/health')
 def health():
-    return jsonify({"status": "healthy"}), 200
+    return "OK", 200
 
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if not data:
+        return jsonify({"error": "No se proporcionaron datos"}), 400
+    try:
+        #converitr json a dataframe
+        df = pd.DataFrame[(data)]
+        
+        #escalar los datos
+        df_scaled = scaler.transform(df)
+        
+        #predecir con el modelo 0 = Normal, 1 = Ataque
+        prediction = model.predict(df_scaled)[0]
+        
+        return jsonify({
+            "status": "success",
+            "prediction": int(prediction),
+            "action" : "mutate" if prediction == 1 else "no action"
+        })
+        
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host = '0.0.0.0', port = 8080)
--- terraform/main.tf
+++ terraform/main.tf
@@ -253,3 +253,47 @@ output "ssh_command" {
 output "current_mtd_port" {
   value = var.mtd_active_ports
 }
+
+
+
+
+
+
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acrGhostNodeProd"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic" 
+  admin_enabled       = false
+}
+
+
+resource "azurerm_role_assignment" "aks_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-mtd-${random_id.server_suffix.hex}"
+  location            = azurerm_resource_group.honeypot_rg.location
+  resource_group_name = azurerm_resource_group.honeypot_rg.name
+  dns_prefix          = "mtd-aks"
+  kubernetes_version  = "1.27"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+}
